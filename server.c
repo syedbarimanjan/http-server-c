e0b088e009c9e57dbb8e442c8774b39e403ce07e
syedbarimanjan <syedbarimanjan@gmail.com>
2025-12-01 02:08:36 +0500

    Add bind and listen with some testing

--- server.c
+++ server.c
@@ -18,9 +18,25 @@ struct Server server_constructor(int domain, int service, int protocol, u_long i
   server.address.sin_addr.s_addr = htonl(interface);
 
   server.socket = socket(domain, service, protocol);
-  if(server.socket == 0){
+  if(server.socket == 0)
+  {
       perror("Failed to connect to socket...\n");
       exit(1);
   }
+
+  if((bind(server.socket, (struct sockaddr *)&server.address, sizeof(server.address))) < 0)
+  {
+      perror("Failed to bind to socket...\n");
+      exit(1);
+  }
+
+  if((listen(server.socket,server.backlog)) < 0)
+  {
+      perror("Failed to listen to socket...\n");
+      exit(1);
+  }
+
+  server.launch = launch;
+
   return server;
 };
