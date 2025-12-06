6080d124bf9a1d4c5e2a65640375a6851902801f
syedbarimanjan <syedbarimanjan@gmail.com>
2025-12-06 07:54:27 +0500

    test the current implementation and we have a simple server up and running :)

--- server.h
+++ server.h
@@ -18,7 +18,7 @@ struct Server
 
     int socket;
 
-    void(*launch)(void);
+    void(*launch)(struct Server *server);
 };
 
 struct Server server_constructor(int domain, int service, int protocol, u_long interface, int port, int backlog, void(*launch)(struct Server *server));
--- test.c
+++ test.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include "server.h"
 
 void launch(struct Server *server)
 {
-    char buffer[30000];
-    printf("=============== WAITING for CONNECTION ===============");
-    int address_length = sizeof(server->address);
-    int new_socket = accept(server->socket, (struct sockaddr *)&server->address , (socklen_t *)&address_length);
-    read(new_socket,buffer,30000);
+    while(1){
+        char buffer[30000];
+        printf("=============== WAITING for CONNECTION ===============\n");
+        int address_length = sizeof(server->address);
+        int new_socket = accept(server->socket, (struct sockaddr *)&server->address , (socklen_t *)&address_length);
+        read(new_socket,buffer,30000);
+        printf("%s\n", buffer);
+        char *hello = "HTTP/1.1 200 OK\r\nDate: Mon, 27 Jul 2009 12:28:53 GMT\r\nServer: Apache/2.2.14 (Win32)\r\nLast-Modified: Wed, 22 Jul 2009 19:15:56 GMT\r\nContent-Length: 45\r\nContent-Type: text/html\r\nConnection: Closed\r\n\r\n<html><body><h1>Hello World</h1></body></html>";
+        write(new_socket,hello,strlen(hello));
+        close(new_socket);
+    }
 }
 
 int main()
 {
-    struct Server server = server_constructor(AF_INET,SOCK_STREAM,0,INADDR_ANY,80,10);
+    struct Server server = server_constructor(AF_INET,SOCK_STREAM,0,INADDR_ANY,8080,10,launch);
+    server.launch(&server);
 }
