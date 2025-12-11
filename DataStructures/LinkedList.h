30c4711b38bffd932ea22c4845846d214c2c2b7c
syedbarimanjan <syedbarimanjan@gmail.com>
2025-12-11 16:36:24 +0500

    create a linkedlist for future use

---
+++ DataStructures/LinkedList.h
@@ -0,0 +1,18 @@
+#ifndef LinkedList_h
+#define LinkedList_h
+
+#include "Node.h"
+#include <stdlib.h>
+
+struct LinkedList_int {
+    struct Node_int *head;
+    int length;
+
+    void(*insert)(int index, int data, struct LinkedList_int *linked_list);
+    void(*remove)(int index, struct LinkedList_int *linked_list);
+    int(*retrieve)(int index, struct LinkedList_int *linked_list);
+};
+
+struct LinkedList_int linked_list_int_constructor();
+
+#endif
